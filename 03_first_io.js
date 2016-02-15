fd31ac1d252e16020c7578beaf355584a3111aa5
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-15 21:39:16 +0000

    Finished exercise seven.

--- 03_first_io.js
+++ 03_first_io.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
-var file = process.argv[2];
 
+var file = process.argv[2];
 file = fs.readFileSync(file);
 
 var contents = file.toString();
 
 console.log(contents.split('\n').length - 1);
+
